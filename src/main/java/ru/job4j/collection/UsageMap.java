5eef5f1445884ede5721b29891b3169e527416f4
master <yuriy_frolov_2019@mail.ru>
2020-08-07 18:19:48 +0300

    0. Map, HashMap [#331326]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Fro1993@mail.ru","Frolov Yuriy Olegovich");
+        for (Map.Entry<String, String> entry:
+             map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+}
