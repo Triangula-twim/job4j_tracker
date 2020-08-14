bc9181359e5502acba61cf558b2523813c3ed097
master <yuriy_frolov_2019@mail.ru>
2020-08-14 11:39:33 +0300

    4. Преобразование List в Map. [#331169]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -1,11 +1,17 @@
 package ru.job4j.stream;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class School {
     public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
         return students.stream().filter(predict).collect(Collectors.toList());
+    }
+
+    public static Map<String, Student> listToMap(List<Student> students) {
+        return students.stream().collect(Collectors.toMap(Student::getSurname,
+                student -> student));
     }
 }
--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 
 public class SchoolTest {
 
@@ -38,5 +39,18 @@ public class SchoolTest {
         List<Student> expected = List.of(new Student("Vinokuriv", 71));
         Assert.assertEquals(School.collect(list, (x) ->
                 x.getScore() >= 70 && x.getScore() <= 100), expected);
+    }
+
+    @Test
+    public void checkMap() {
+        List<Student> list = List.of(new Student("Firsov", 56),
+                new Student("Frolov", 39),
+                new Student("Vinokuriv", 71),
+                new Student("Lyapushkin", 65));
+        Map<String, Student> expected = Map.of("Firsov", new Student("Firsov", 56),
+                "Frolov", new Student("Frolov", 39),
+                "Vinokuriv", new Student("Vinokuriv", 71),
+                "Lyapushkin", new Student("Lyapushkin", 65));
+        Assert.assertEquals(School.listToMap(list), expected);
     }
 }
