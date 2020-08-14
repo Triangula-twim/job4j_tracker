d01de28c69deaafe88a6eb48459b229d16bb20f5
master <yuriy_frolov_2019@mail.ru>
2020-08-14 09:45:46 +0300

    1. Фильтрация учеников [#331166] (доп. 1)

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 public class School {
     public static List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        return students.stream().filter(student ->
-                predict.test(student)).collect(Collectors.toList());
+        return students.stream().filter(predict).collect(Collectors.toList());
     }
 }
